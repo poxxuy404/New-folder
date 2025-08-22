30d18a4f5d53f41432372630fb1e2d23ae57465e
poxuyy404 <bunyodqosimov48@gmail.com>
2025-08-22 04:48:38 -0700

    https

--- app.js
+++ app.js
@@ -1,3 +1,71 @@
-let a = "bunyod bugun darsda bu masalani topdi va u maladest bola "
-let textReverce = " "
-for ( )
+// 1) 20 dan -20 gacha bo‘lgan sonlar
+for (let i = 20; i >= -20; i--) {
+    console.log(i);
+}
+
+// 2) 30 dan 90 gacha 3 ga va 5 ga bo‘linadigan sonlar
+for (let i = 30; i <= 90; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(i);
+    }
+}
+
+// 3) 1 dan 20 gacha 2 ga ham, 3 ga ham bo‘linmaydigan sonlar
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 !== 0 && i % 3 !== 0) {
+        console.log(i);
+    }
+}
+
+// 4) Text ichidagi unli harflar soni
+let text1 = "Bugun javascript mashqlarini bajarayapmiz"
+let countUnli = 0
+let unliHarflar = "aeiouAEIOU"
+
+for (let i = 0; i < text1.length; i++) {
+    if (unliHarflar.includes(text1[i])) {
+        countUnli++
+    }
+}
+console.log("Unli harflar soni: " + countUnli);
+
+// 5) Gap ichidan raqamlarni ajratib olish
+let text2 = "Bugun 3-ta mashq 5 daqiqada bajarildi va 100% topshirildi"
+let raqamlar = ""
+
+for (let i = 0; i < text2.length; i++) {
+    if (!isNaN(text2[i]) && text2[i] !== ' ') {
+        raqamlar += text2[i]
+    }
+}
+console.log("Raqamlar: " + raqamlar);
+
+// 6) Gapni ichidan unlilar, undoshlar, bosh joy, belgi va raqamlarni ajratish
+let text3 = "Salom, bu 2025-yilning eng yaxshi  kunlaridan biri!"
+let unli = ""
+let undosh = ""
+let probel = ""
+let belgi = ""
+let raqam = ""
+
+for (let i = 0; i < text3.length; i++) {
+    let char = text3[i]
+
+    if ("aeiouAEIOU".includes(char)) {
+        unli += char
+    } else if ("bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ".includes(char)) {
+        undosh += char
+    } else if (char === " ") {
+        probel += char
+    } else if (!isNaN(char) && char !== " ") {
+        raqam += char
+    } else {
+        belgi += char
+    }
+}
+
+console.log("Unlilar: " + unli);
+console.log("Undoshlar: " + undosh);
+console.log("Bo'sh joylar soni: " + probel.length);
+console.log("Belgilar: " + belgi);
+console.log("Raqamlar: " + raqam);
